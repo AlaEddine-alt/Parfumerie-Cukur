f02427264ee5c1b730311c62bce06884bbc65818
Ala Eddine <alabenmimoun1@yahoo.com>
2024-02-13 11:57:42 +0100

    update createCommand

--- server/api/models/orderitem.js
+++ server/api/models/orderitem.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 const orderitemSchema = new mongoose.Schema({
-    _id_orderitem: mongoose.Schema.Types.ObjectId,
-    _id_product: { type: mongoose.Schema.Types.ObjectId, ref: 'product', required: true },
-    _id_command: { type: mongoose.Schema.Types.ObjectId, ref: 'command', required: true },
-    quantity : {type : Number , required : true}
-    
-});                                                                                                                     
+    _id: mongoose.Schema.Types.ObjectId,
+    _id_product: { type: mongoose.Schema.Types.ObjectId, ref: 'product' },
+    _id_command: { type: mongoose.Schema.Types.ObjectId, ref: 'command' },
+    quantity: { type: Number }
+
+});
 
 
 const Orderitem = mongoose.model('Orderitem', orderitemSchema);
--- server/app.js
+++ server/app.js
@@ -4,10 +4,10 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/product');
-const commandRoutes = require ('./api/routes/command');
+const commandRoutes = require('./api/routes/command');
 
 
-//mongoose.connect('mongodb+srv://alaeddinebenmimoun:2HLVGkii4th2Y8xj@cluster0.alwpmdy.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://alaeddinebenmimoun:2HLVGkii4th2Y8xj@cluster0.alwpmdy.mongodb.net/?retryWrites=true&w=majority');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
